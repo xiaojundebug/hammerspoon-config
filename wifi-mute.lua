86f855c972baedc658005c97a8c986ea02b3262f
imzxj <362896731@qq.com>
2023-07-20 10:34:27 +0800

    change paste shortcut key

--- wifi-mute.lua
+++ wifi-mute.lua
@@ -5,12 +5,6 @@
 -- 定义公司的 Wi-Fi 名称
 local companyWifi = 'MUDU'
 
--- 检测当前 Wi-Fi 的名称
-local function getCurrentWifiName()
-  local wifiInfo = hs.wifi.currentNetwork()
-  return wifiInfo or ""
-end
-
 -- 静音函数
 local function muteVolume()
   hs.audiodevice.defaultOutputDevice():setOutputMuted(true)
@@ -23,7 +17,7 @@ end
 
 -- 监听 Wi-Fi 变化
 wifiWatcher = hs.wifi.watcher.new(function()
-  local currentWifi = getCurrentWifiName()
+  local currentWifi = hs.wifi.currentNetwork()
   if currentWifi == companyWifi then
     muteVolume() -- 如果是公司 Wi-Fi，则静音
   else
